e9868c86d951770071dc8591f3e6a4e2535a0f76
shivasapra <ashishsapra7@gmail.com>
2019-07-26 15:44:24 +0530

    Maintaing Status Of Donation

--- resources/views/contribution/packages.blade.php
+++ resources/views/contribution/packages.blade.php
@@ -9,7 +9,6 @@
             <a class="nav-link" href="{{route('contribution.donations',Auth::user())}}">My Contribution/Donation</a>
         </li>
     </ul>
-    
     @foreach ($packages->reverse()->chunk(3) as $chunk)
         <div class="row">
             @foreach ($chunk->reverse() as $package)
@@ -18,7 +17,19 @@
                     <div class="media overflow-visible">
                         <div class="media-body media-middle overflow-visible">
                             <div class="heading-tag @if($package->package == 'STANDARD') standard-gradient @elseif($package->package == 'Premium') premium-gradient @endif">{{$package->package}}</div>
-                            <h2>INR {{number_format( (float) ($package->amount), 2, '.', '')}}</h2>
+                            <h2>INR {{$package->amount}}</h2>
+                            @if($user->donations->count() == 0 and $package->level == 1)
+                                <span class="badge bg-info">Contribute Now</span>
+                            @endif
+                            @if($user->donations()->where('package',$package->package)->orderBy('id','desc')->first() != null)
+                                @if($user->donations()->where('package',$package->package)->orderBy('id','desc')->first()->level == $package->level)
+                                    <span class="badge bg-warning">Current Status</span>
+                                @endif
+                                @if($user->donations()->where('package',$package->package)->pluck('level')->contains($package->level))
+                                    <h6 class="text-muted">{{$user->donations()->where('package',$package->package)->where('level',$package->level)->first()->created_at}}</h6>
+                                @endif
+                            @endif
+                            
                         </div>
                         <div class="media-right">
                             <img src="{{asset('app/images/medal'.$package->level.'.png')}}" alt="gold medal" class="media-object"/>
