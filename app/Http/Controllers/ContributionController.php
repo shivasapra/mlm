a7f280ced6961f0bf6c9bd0274e812b6c9232d6a
shivasapra <ashishsapra7@gmail.com>
2019-07-26 14:39:04 +0530

    Fetching Data From Contribution Packages and displaying It

--- app/Http/Controllers/ContributionController.php
+++ app/Http/Controllers/ContributionController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\ContributionPackages;
 
 class ContributionController extends Controller
 {
     public function packages(User $user){
-        return view('contribution.packages')->with('user',$user);
+        return view('contribution.packages')->with('user',$user)->with('packages',ContributionPackages::all());
     }
 
     public function donations(User $user){
