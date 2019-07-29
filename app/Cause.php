e7e43c7b508e6da7dbe0d219d91de12275389cf3
shivasapra <ashishsapra7@gmail.com>
2019-07-29 18:17:14 +0530

    Causes Table

--- app/Cause.php
+++ app/Cause.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Settings extends Model
+class Cause extends Model
 {
-    protected $table = 'settings';
+    protected $table = 'causes';
 }
