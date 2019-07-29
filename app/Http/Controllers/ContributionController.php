73327ae8b652b7217dd42c9856804dc86922f55a
shivasapra <ashishsapra7@gmail.com>
2019-07-29 14:57:49 +0530

    Mail To Parent

--- app/Http/Controllers/ContributionController.php
+++ app/Http/Controllers/ContributionController.php
@@ -55,6 +55,14 @@ class ContributionController extends Controller
                 $super_parent_user = User::find($coordinates->super_parent);
                 $super_parent_user->coordinates->super_children = ($super_parent_user->coordinates->super_children == null) ? Auth::user()->id : $super_parent_user->coordinates->super_children.','.Auth::user()->id;
                 $super_parent_user->coordinates->save();
+
+                $super_parent_amount = (70/100) * $donation->amount;
+                $data = ['super_parent_user' => $super_parent_user, 'user' => Auth::user(), 'amount'=> $super_parent_amount];
+                $contactEmail = $super_parent_user->email;
+                Mail::send('emails.superParents', $data, function($message) use ($contactEmail)
+                {  
+                    $message->to($contactEmail)->subject('Contribution Amount!!');
+                });
             }
 
             $parent_amount = (25/100) * $donation->amount;
@@ -63,6 +71,15 @@ class ContributionController extends Controller
             Mail::send('emails.parents', $data, function($message) use ($contactEmail)
             {  
                 $message->to($contactEmail)->subject('Contribution Amount!!');
+            });
+
+
+
+            $data = ['user'=>Auth::user()];
+            $contactEmail = Auth::user()->email;
+            Mail::send('emails.thankYou', $data, function($message) use ($contactEmail)
+            {  
+                $message->to($contactEmail)->subject('Thankyou');
             });
         }
         return redirect()->back();
