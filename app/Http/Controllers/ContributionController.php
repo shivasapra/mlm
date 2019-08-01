d4dd3433decad90aeb086b31aaeefb9727610e29
shiva <ashishsapra7@gmail.com>
2019-08-01 13:38:43 +0530

    Implementing Level 3

--- app/Http/Controllers/ContributionController.php
+++ app/Http/Controllers/ContributionController.php
@@ -27,10 +27,38 @@ class ContributionController extends Controller
         $donation->package = $request->package;
         $donation->amount = $request->amount;
         $donation->save();
+        if($parent_user = Details::where('username',Auth::user()->details->invited_by)->first()->user){
+            $parent_amount = Settings::first()->level_two_percentage;
+            $data = ['parent_user' => $parent_user, 'user' => Auth::user(), 'amount'=> $parent_amount];
+            $contactEmail = $parent_user->email;
+            Mail::send('emails.parents', $data, function($message) use ($contactEmail)
+            {  
+                $message->to($contactEmail)->subject('Contribution Amount!!');
+            });
+
+            if($super_parent_user = User::find($parent_user->coordinates->parent)){
+                $super_parent_amount = Settings::first()->level_one_percentage;
+                $data = ['super_parent_user' => $super_parent_user, 'user' => Auth::user(), 'amount'=> $super_parent_amount];
+                $contactEmail = $super_parent_user->email;
+                Mail::send('emails.superParents', $data, function($message) use ($contactEmail)
+                {  
+                    $message->to($contactEmail)->subject('Contribution Amount!!');
+                });
+                
+                if($super_duper_parent_user = User::find($super_parent_user->coordinates->parent)){
+                    //
+                }
+            }
+        }
+
+        $data = ['user'=>Auth::user()];
+        $contactEmail = Auth::user()->email;
+        Mail::send('emails.thankYou', $data, function($message) use ($contactEmail)
+        {  
+            $message->to($contactEmail)->subject('Thankyou');
+        });
 
         if(Auth::user()->coordinates == null){
-            $parent_user = Details::where('username',Auth::user()->details->invited_by)->first()->user;
-
             $coordinates = new Coordinates;
             $coordinates->user_id = Auth::user()->id;
             $coordinates->row = $parent_user->coordinates->row + 1 ;
@@ -43,43 +71,24 @@ class ContributionController extends Controller
                 $coordinates->column = $parent_user->coordinates->column + count(explode(',',$parent_user->coordinates->children)) + (-2);
                 $coordinates->self_position_wrt_parent = count(explode(',',$parent_user->coordinates->children)) + 1;
             }
-            $coordinates->super_parent = $parent_user->coordinates->parent;
             $coordinates->parent = $parent_user->id;
             $coordinates->save();
-
+            
             $parent_user->coordinates->children = ($parent_user->coordinates->children == null) ? Auth::user()->id : $parent_user->coordinates->children.','.Auth::user()->id;
             $parent_user->coordinates->save();
-
-            if($coordinates->super_parent != null){
-                $super_parent_user = User::find($coordinates->super_parent);
+            
+            if($super_parent_user != null){
+                $coordinates->super_parent = $super_parent_user->id;
                 $super_parent_user->coordinates->super_children = ($super_parent_user->coordinates->super_children == null) ? Auth::user()->id : $super_parent_user->coordinates->super_children.','.Auth::user()->id;
                 $super_parent_user->coordinates->save();
-
-                $super_parent_amount = Settings::first()->level_one_percentage;
-                $data = ['super_parent_user' => $super_parent_user, 'user' => Auth::user(), 'amount'=> $super_parent_amount];
-                $contactEmail = $super_parent_user->email;
-                Mail::send('emails.superParents', $data, function($message) use ($contactEmail)
-                {  
-                    $message->to($contactEmail)->subject('Contribution Amount!!');
-                });
             }
 
-            $parent_amount = Settings::first()->level_two_percentage;
-            $data = ['parent_user' => $parent_user, 'user' => Auth::user(), 'amount'=> $parent_amount];
-            $contactEmail = $parent_user->email;
-            Mail::send('emails.parents', $data, function($message) use ($contactEmail)
-            {  
-                $message->to($contactEmail)->subject('Contribution Amount!!');
-            });
-
-
-
-            $data = ['user'=>Auth::user()];
-            $contactEmail = Auth::user()->email;
-            Mail::send('emails.thankYou', $data, function($message) use ($contactEmail)
-            {  
-                $message->to($contactEmail)->subject('Thankyou');
-            });
+            if($super_duper_parent_user != null){
+                $coordinates->super_duper_parent = $super_duper_parent_user->id;
+                $super_duper_parent_user->coordinates->super_duper_children = ($super_duper_parent_user->coordinates->super_duper_children == null) ? Auth::user()->id : $super_duper_parent_user->coordinates->super_duper_children.','.Auth::user()->id;
+                $super_duper_parent_user->coordinates->save();
+                $coordinates->save();
+            }
         }
         return redirect()->back();
     }
--- database/migrations/2019_07_27_053550_create_coordinates_table.php
+++ database/migrations/2019_07_27_053550_create_coordinates_table.php
@@ -18,11 +18,13 @@ class CreateCoordinatesTable extends Migration
             $table->integer('user_id');
             $table->integer('row')->nullable(); // row coordinate 
             $table->integer('column')->nullable(); // column coordinate
+            $table->integer('super_duper_parent')->nullable(); //user_id of superDuperParent
             $table->integer('super_parent')->nullable(); //user_id of superparent
             $table->integer('parent')->nullable(); //user_id of parent
             $table->integer('self_position_wrt_parent')->nullable(); // SELF POSITION W.R.TO PARENT i.e 1 or 2 or 3 or 4 or 5
             $table->string('children')->nullable(); // This will be the array of user_id's of this users's children  separated bt commas i.e [2,4,6,8,8] maximum 5 id's
             $table->string('super_children')->nullable(); // This will be the array of user_id's of this users's children's children i.e [2,4,6,8,8,...] maximum 25 id's
+            $table->longText('super_duper_children')->nullable(); // This will be the array of user_id's of this users's children's children i.e [2,4,6,8,8,...] maximum 125 id's
             $table->timestamps();
         });
     }
