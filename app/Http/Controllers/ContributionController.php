81c998216561a08da719dbdcaf9670b8bce198ab
shiva <ashishsapra7@gmail.com>
2019-08-01 13:55:26 +0530

    Implementing Level 3

--- app/Http/Controllers/ContributionController.php
+++ app/Http/Controllers/ContributionController.php
@@ -26,37 +26,39 @@ class ContributionController extends Controller
         $donation->user_id = Auth::user()->id;
         $donation->package = $request->package;
         $donation->amount = $request->amount;
-        $donation->save();
-        if($parent_user = Details::where('username',Auth::user()->details->invited_by)->first()->user){
-            $parent_amount = Settings::first()->level_two_percentage;
-            $data = ['parent_user' => $parent_user, 'user' => Auth::user(), 'amount'=> $parent_amount];
-            $contactEmail = $parent_user->email;
-            Mail::send('emails.parents', $data, function($message) use ($contactEmail)
-            {  
-                $message->to($contactEmail)->subject('Contribution Amount!!');
-            });
+        
+        if(Auth::id() != 1){
+            if($parent_user = Details::where('username',Auth::user()->details->invited_by)->first()->user){
+                $parent_amount = Settings::first()->level_two_percentage;
+                $data = ['parent_user' => $parent_user, 'user' => Auth::user(), 'amount'=> $parent_amount];
+                $contactEmail = $parent_user->email;
+                // Mail::send('emails.parents', $data, function($message) use ($contactEmail)
+                // {  
+                //     $message->to($contactEmail)->subject('Contribution Amount!!');
+                // });
 
-            if($super_parent_user = User::find($parent_user->coordinates->parent)){
-                $super_parent_amount = Settings::first()->level_one_percentage;
-                $data = ['super_parent_user' => $super_parent_user, 'user' => Auth::user(), 'amount'=> $super_parent_amount];
-                $contactEmail = $super_parent_user->email;
-                Mail::send('emails.superParents', $data, function($message) use ($contactEmail)
-                {  
-                    $message->to($contactEmail)->subject('Contribution Amount!!');
-                });
-                
-                if($super_duper_parent_user = User::find($super_parent_user->coordinates->parent)){
-                    //
+                if($super_parent_user = User::find($parent_user->coordinates->parent)){
+                    $super_parent_amount = Settings::first()->level_one_percentage;
+                    $data = ['super_parent_user' => $super_parent_user, 'user' => Auth::user(), 'amount'=> $super_parent_amount];
+                    $contactEmail = $super_parent_user->email;
+                    // Mail::send('emails.superParents', $data, function($message) use ($contactEmail)
+                    // {  
+                    //     $message->to($contactEmail)->subject('Contribution Amount!!');
+                    // });
+
+                    if($super_duper_parent_user = User::find($super_parent_user->coordinates->parent)){
+                        //
+                    }
                 }
             }
         }
 
         $data = ['user'=>Auth::user()];
         $contactEmail = Auth::user()->email;
-        Mail::send('emails.thankYou', $data, function($message) use ($contactEmail)
-        {  
-            $message->to($contactEmail)->subject('Thankyou');
-        });
+        // Mail::send('emails.thankYou', $data, function($message) use ($contactEmail)
+        // {  
+        //     $message->to($contactEmail)->subject('Thankyou');
+        // });
 
         if(Auth::user()->coordinates == null){
             $coordinates = new Coordinates;
@@ -72,24 +74,24 @@ class ContributionController extends Controller
                 $coordinates->self_position_wrt_parent = count(explode(',',$parent_user->coordinates->children)) + 1;
             }
             $coordinates->parent = $parent_user->id;
-            $coordinates->save();
             
             $parent_user->coordinates->children = ($parent_user->coordinates->children == null) ? Auth::user()->id : $parent_user->coordinates->children.','.Auth::user()->id;
             $parent_user->coordinates->save();
             
-            if($super_parent_user != null){
+            if($super_parent_user){
                 $coordinates->super_parent = $super_parent_user->id;
                 $super_parent_user->coordinates->super_children = ($super_parent_user->coordinates->super_children == null) ? Auth::user()->id : $super_parent_user->coordinates->super_children.','.Auth::user()->id;
                 $super_parent_user->coordinates->save();
             }
 
-            if($super_duper_parent_user != null){
+            if($super_duper_parent_user){
                 $coordinates->super_duper_parent = $super_duper_parent_user->id;
                 $super_duper_parent_user->coordinates->super_duper_children = ($super_duper_parent_user->coordinates->super_duper_children == null) ? Auth::user()->id : $super_duper_parent_user->coordinates->super_duper_children.','.Auth::user()->id;
                 $super_duper_parent_user->coordinates->save();
-                $coordinates->save();
             }
+            $coordinates->save();
         }
+        $donation->save();
         return redirect()->back();
     }
 }
