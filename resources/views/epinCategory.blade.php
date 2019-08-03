e0b89efa45c8b8c87c5e6e5a531742a912cbf112
shivasapra <ashishsapra7@gmail.com>
2019-08-03 18:32:41 +0530

    Transfer History

--- resources/views/epinCategory.blade.php
+++ resources/views/epinCategory.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.app', ['titlePage' => __('Epin Category Details')])
+<?php use App\User; ?>
 @section('content-body')
 <h1>{{$category->name}}</h1><hr>
 <table class="table table-bordered">
@@ -42,10 +43,77 @@
                     @endif
                 </td>
                 <td class="text-center">
-                    <button class="btn btn-sm btn-info">View</button>
+                    <button class="btn btn-sm btn-info" onclick="history(this)">View
+                        <input type="hidden" class="epin_id" value={{$epin->id}}>
+                    </button>
                 </td>
             </tr>
         @endforeach
     </tbody>
 </table>
+<button type="button" id="modalButton" data-toggle="modal" data-target="#transferModal" style="display:none;"></button>
+<div id="modalDisplay"></div>
 @endsection
+@section('js')
+    <script>
+        function history(temp){
+            
+            var epin_id = $(temp).find('.epin_id').val();
+            @foreach(App\Epin::all() as $epin)
+            var fetched_epin_id = {!! json_encode($epin->id) !!}
+                if(fetched_epin_id == epin_id){
+                    var modal = 
+                    '<div class="modal fade" id="transferModal">'+
+                        '<div class="modal-dialog">'+
+                            '<div class="modal-content">'+
+                                '<!-- Modal Header -->'+
+                                '<div class="modal-header">'+
+                                '<h4 class="modal-title">Epin Sent By Admin To {{User::find($epin->sent_to)->details->username}}</h4>'+
+                                '<button type="button" class="close" data-dismiss="modal">&times;</button>'+
+                                '</div>'+
+                                '<!-- Modal body -->'+
+                                '<div class="modal-body">'+
+                                        '<table class="table table-bordered">'+
+                                            '<thead>'+
+                                                '<tr>'+
+                                                    '<th>Sno.</th>'+
+                                                    '<th>Transferred By</th>'+
+                                                    '<th>Transferred To</th>'+
+                                                    '<th>Transferred On</th>'+
+                                                '</tr>'+
+                                            '</thead>'+
+                                            '<tbody>'+
+                                                '<?php $j = 1;?>'+
+                                                
+                                                        '@foreach($epin->transfers as $transfer)'+
+                                                        '<tr>'+
+                                                            '<td><b>{{$j++}}</b></td>'+
+                                                            '<td>{{User::find($transfer->from)->details->username}}</td>'+
+                                                            '<td>{{User::find($transfer->to)->details->username}}</td>'+
+                                                            '<td>{{$transfer->created_at}}</td>'+
+                                                        '</tr>'+
+                                                        '@endforeach'+
+                                                    
+                                            '</tbody>'+
+                                            '</table>'+
+                                '</div>'+
+                        
+                                '<!-- Modal footer -->'+
+                                '<div class="modal-footer">'+
+                                '<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>'+
+                                '</div>'+
+                            '</div>'+
+                        '</div>'+
+                    '</div>';
+                    $('#modalDisplay').html(modal);
+                    $('#modalButton').click();
+                }
+          @endforeach
+        }
+
+
+  
+        
+	  
+    </script>
+@stop
