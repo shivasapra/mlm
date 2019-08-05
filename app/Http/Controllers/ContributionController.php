e611235d49a875bd5caac7ca9a3f61aee973a932
shivasapra <ashishsapra7@gmail.com>
2019-08-05 16:19:15 +0530

    Contribution Controller Changes

--- app/Http/Controllers/ContributionController.php
+++ app/Http/Controllers/ContributionController.php
@@ -145,7 +145,18 @@ class ContributionController extends Controller
     }
 
     // private function sendMail($data, $contactEmail){
-        
+    //     $parent_amount = Settings::first()->level_three_percentage;
+    //     $data = ['name' => $parent_user->name, 'user' => Auth::user(), 'amount'=> $parent_amount];
+    //     $contactEmail = $parent_user->email;
+    //     $this->sendMail($data ,$contactEmail);
+    //     $super_parent_amount = Settings::first()->level_two_percentage;
+    //     $data = ['name' => $super_parent_user->name, 'user' => Auth::user(), 'amount'=> $super_parent_amount];
+    //     $contactEmail = $super_parent_user->email;
+    //     $this->sendMail($data ,$contactEmail);
+    //     $super_duper_parent_amount = Settings::first()->level_one_percentage;
+    //     $data = ['name' => $super_duper_parent_user->name, 'user' => Auth::user(), 'amount'=> $super_duper_parent_amount];
+    //     $contactEmail = $super_duper_parent_user->email;
+    //     $this->sendMail($data ,$contactEmail);
     //     Mail::send('emails.contribution', $data, function($message) use ($contactEmail)
     //         {  
     //             $message->to($contactEmail)->subject('Contribution Amount!!');
