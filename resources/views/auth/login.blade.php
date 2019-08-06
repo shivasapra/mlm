7ca9323f857398c519a4b127383784c4deb1e61b
shivasapra <ashishsapra7@gmail.com>
2019-08-06 10:27:23 +0530

    Login Title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html lang="en">
 	<head>
-		<title>Login: MLM</title>
+		<title>Login:</title>
 		<!-- Required meta tags -->
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
