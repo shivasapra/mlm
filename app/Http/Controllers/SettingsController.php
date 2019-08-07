d8cd998008dbb78307407e56fe816aa37c6a5e63
shivasapra <ashishsapra7@gmail.com>
2019-08-07 10:26:10 +0530

    Settings Changes

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -16,22 +16,21 @@ class SettingsController extends Controller
 
     public function save(Request $request){
         if(Settings::first() == null){
-            $settings = new Settings;
+            Settings::create($request->all());
         }else{
             $settings = Settings::first();
         }
+        // $settings->level_one_percentage = $request->level_one_percentage;
+        // $settings->level_two_percentage = $request->level_two_percentage;
+        // $settings->level_three_percentage = $request->level_three_percentage;
+        // $settings->admin_amount = $request->admin_amount;
+        // $settings->basic_amount = $request->basic_amount;
 
-        $settings->level_one_percentage = $request->level_one_percentage;
-        $settings->level_two_percentage = $request->level_two_percentage;
-        $settings->level_three_percentage = $request->level_three_percentage;
-        $settings->admin_amount = $request->admin_amount;
-        $settings->basic_amount = $request->basic_amount;
-
-        $settings->level_one_percentage_standard = $request->level_one_percentage_standard;
-        $settings->level_two_percentage_standard = $request->level_two_percentage_standard;
-        $settings->level_three_percentage_standard = $request->level_three_percentage_standard;
-        $settings->admin_amount_standard = $request->admin_amount_standard;
-        $settings->standard_amount = $request->standard_amount;
+        // $settings->level_one_percentage_standard = $request->level_one_percentage_standard;
+        // $settings->level_two_percentage_standard = $request->level_two_percentage_standard;
+        // $settings->level_three_percentage_standard = $request->level_three_percentage_standard;
+        // $settings->admin_amount_standard = $request->admin_amount_standard;
+        // $settings->standard_amount = $request->standard_amount;
         $settings->save();
 
         Session::flash('success','Saved!!');
