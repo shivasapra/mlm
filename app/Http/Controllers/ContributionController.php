1327590e04c8e9c1d49234bd48e26896f0d98fb8
shivasapra <ashishsapra7@gmail.com>
2019-08-09 17:24:42 +0530

    Adding 3rd Layer

--- app/Http/Controllers/ContributionController.php
+++ app/Http/Controllers/ContributionController.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\User;
 use App\ContributionPackages;
@@ -16,34 +14,27 @@ use Carbon\Carbon;
 use App\Transfer;
 use Session;
 use App\Commision;
-
 class ContributionController extends Controller
 {
     public function packages(User $user){
         return view('contribution.packages')->with('user',$user)->with('packages',ContributionPackages::all());
     }
-
     public function donations(User $user){
         return view('contribution.donations')->with('user',$user);
     }
-
     public function contribute(Request $request, Donation $donation){
         
         $epin = $this->verifyEpin($request);
         $d = $this->donate($request, $donation);
         
-
         if(!Auth::user()->admin and Auth::user()->coordinates == null){
             $parent_user = $this->findParentUser();
             $coordinates = $this->setCoordinates($parent_user);
         }
-
         
         
         return redirect()->back();
     }
-
-
     private function verifyEpin($request){
         $epin = Epin::where('epin',$request->epin)->first();
         if(!$epin->count() or $epin->used_by != null){
@@ -56,10 +47,8 @@ class ContributionController extends Controller
         $epin->used_by = Auth::user()->id;
         $epin->used_at = Carbon::now();
         $epin->save();
-
         return $epin;
     }
-
     private function donate($request, $donation){
         $donation->user_id = Auth::user()->id;
         $donation->package = $request->package;
@@ -70,10 +59,8 @@ class ContributionController extends Controller
         $contactEmail = User::where('admin',1)->first()->email;
         $this->sendMail($data ,$contactEmail);
         $this->commission(Settings::first()->admin_amount,User::where('admin',1)->first());
-
         return $donation;
     }
-
     private function findParentUser(){
         $temp = Details::where('username',Auth::user()->details->invited_by)->first()->user;
         if(count(explode(',',$temp->coordinates->children)) < 5){
@@ -97,13 +84,11 @@ class ContributionController extends Controller
         $commission->from = Auth::id();
         $commission->save();
     }
-
     private function setCoordinates($parent_user){
         $coordinates = new Coordinates;
         $coordinates->user_id = Auth::user()->id;
         $coordinates->row = $parent_user->coordinates->row + 1 ;
         $coordinates->parent = $parent_user->id;
-
         if($parent_user->coordinates->children == null ){
             $coordinates->column = $parent_user->coordinates->column - 2 ;
             $coordinates->self_position_wrt_parent = 1;
@@ -113,37 +98,40 @@ class ContributionController extends Controller
         }
         $parent_user->coordinates->children = ($parent_user->coordinates->children == null) ? Auth::user()->id : $parent_user->coordinates->children.','.Auth::user()->id;
         $parent_user->coordinates->save();
-        $parent_amount = Settings::first()->level_two_percentage;
-
+        $parent_amount = Settings::first()->level_three_percentage;
         $data = ['name' => $parent_user->name, 'user' => Auth::user(), 'amount'=> $parent_amount];
         $contactEmail = $parent_user->email;
         $this->sendMail($data ,$contactEmail);
         $this->commission($parent_amount,$parent_user);
         
-
         if($super_parent_user = User::find($parent_user->coordinates->parent)){
             $coordinates->super_parent = $super_parent_user->id;
             $super_parent_user->coordinates->super_children = ($super_parent_user->coordinates->super_children == null) ? Auth::user()->id : $super_parent_user->coordinates->super_children.','.Auth::user()->id;
             $super_parent_user->coordinates->save();
-            $super_parent_amount = Settings::first()->level_one_percentage;
+            $super_parent_amount = Settings::first()->level_two_percentage;
             $data = ['name' => $super_parent_user->name, 'user' => Auth::user(), 'amount'=> $super_parent_amount];
             $contactEmail = $super_parent_user->email;
             $this->sendMail($data ,$contactEmail);
             $this->commission($super_parent_amount,$super_parent_user);
+            if($super_duper_parent_user = User::find($super_parent_user->coordinates->parent)){
+                $coordinates->super_duper_parent = $super_duper_parent_user->id;
+                $super_duper_parent_user->coordinates->super_duper_children = ($super_duper_parent_user->coordinates->super_duper_children == null) ? Auth::user()->id : $super_duper_parent_user->coordinates->super_duper_children.','.Auth::user()->id;
+                $super_duper_parent_user->coordinates->save();
+                $super_duper_parent_amount = Settings::first()->level_one_percentage;
+                $data = ['name' => $super_duper_parent_user->name, 'user' => Auth::user(), 'amount'=> $super_duper_parent_amount];
+                $contactEmail = $super_duper_parent_user->email;
+                $this->sendMail($data ,$contactEmail);
+                $this->commission($super_duper_parent_amount,$super_duper_parent_user);
+            }
         }
         $coordinates->save();
-
         return $coordinates;
     }
-
     
-
     
-
     public function viewer(User $user){
         return view('contribution.viewer')->with('user',$user);
     }
-
     public function UsernameSearch(Request $request){
         if($request->ajax()){
             $output= "";
@@ -156,25 +144,21 @@ class ContributionController extends Controller
             }
         }
     }
-
     private function sendMail($data, $contactEmail){
         Mail::send('emails.contribution', $data, function($message) use ($contactEmail)
             {  
                 $message->to($contactEmail)->subject('Contribution Amount!!');
             });
-
         $data = ['user'=>Auth::user()];
         Mail::send('emails.thankYou', $data, function($message) use ($contactEmail)
             {  
                 $message->to(Auth::user()->email)->subject('Thankyou')->from($contactEmail);
             });
     }
-
     // public function matrix(){
     //     $user = Auth::user();
     //     $arr = array(
     //         $user->name => $this->findChildren($user->id),
-
     //     );
     //     // $array = array(
     //     //     'shiva Sapra'=>['Vineet Chauhan','Aslam Khan'],
@@ -182,7 +166,6 @@ class ContributionController extends Controller
     //     // );
     //     dd($arr);
     // }
-
     // private function findChildren($id){
     //     $children = array();
     //     foreach(collect(explode(',',User::find($id)->coordinates->children)) as $child){
--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -1,110 +1,91 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use App\Settings;
 use App\Cause;
 use Session;
-
 class SettingsController extends Controller
 {
     public function settings(){
         return view('settings')->with('settings',Settings::first())
                                 ->with('causes',Cause::all());
     }
-
     public function saveBasic(Request $request){
         if(!$settings = Settings::first()){
             $settings = new Settings;
         }
-        // if($request->level_one_percentage == 0){
-        //     $settings->level_one_percentage = 0;
-        //     $settings->level_one_percentage_standard = 0;
-        //     $settings->level_one_percentage_premium = 0;
-        // }
-        // else{
-        //     $settings->level_one_percentage = $request->level_one_percentage;
-        // }
-
+        if($request->level_one_percentage == 0){
+            $settings->level_one_percentage = 0;
+            $settings->level_one_percentage_standard = 0;
+            $settings->level_one_percentage_premium = 0;
+        }
+        else{
+            $settings->level_one_percentage = $request->level_one_percentage;
+        }
         
-        $settings->level_one_percentage = $request->level_one_percentage;
         $settings->level_two_percentage = $request->level_two_percentage;
-        // $settings->level_three_percentage = $request->level_three_percentage;
+        $settings->level_three_percentage = $request->level_three_percentage;
         $settings->admin_amount = $request->admin_amount;
         $settings->basic_amount = $request->basic_amount;
         $settings->upgrade_wallet_amount = $request->upgrade_wallet_amount;
         $settings->save();
-
         Session::flash('success','Saved!!');
         return redirect()->back();
     }
-
     public function saveStandard(Request $request){
         if(!$settings = Settings::first()){
             $settings = new Settings;
         }
-        // if($request->level_one_percentage_standard == 0){
-        //     $settings->level_one_percentage = 0;
-        //     $settings->level_one_percentage_standard = 0;
-        //     $settings->level_one_percentage_premium = 0;
-        // }
-        // else{
-        //     $settings->level_one_percentage_standard = $request->level_one_percentage_standard;
-        // }
-
+        if($request->level_one_percentage_standard == 0){
+            $settings->level_one_percentage = 0;
+            $settings->level_one_percentage_standard = 0;
+            $settings->level_one_percentage_premium = 0;
+        }
+        else{
+            $settings->level_one_percentage_standard = $request->level_one_percentage_standard;
+        }
         $settings->level_one_percentage_standard = $request->level_one_percentage_standard;
         $settings->level_two_percentage_standard = $request->level_two_percentage_standard;
-        // $settings->level_three_percentage_standard = $request->level_three_percentage_standard;
+        $settings->level_three_percentage_standard = $request->level_three_percentage_standard;
         $settings->admin_amount_standard = $request->admin_amount_standard;
         $settings->standard_amount = $request->standard_amount;
         $settings->upgrade_wallet_amount_standard = $request->upgrade_wallet_amount_standard;
         $settings->save();
-
         Session::flash('success','Saved!!');
         return redirect()->back();
-
     }
-
     public function savePremium(Request $request){
         if(!$settings = Settings::first()){
             $settings = new Settings;
         }
-
-        // if($request->level_one_percentage_premium == 0){
-        //     $settings->level_one_percentage = 0;
-        //     $settings->level_one_percentage_standard = 0;
-        //     $settings->level_one_percentage_premium = 0;
-        // }
-        // else{
-        //     $settings->level_one_percentage_premium = $request->level_one_percentage_premium;
-        // }
-
+        if($request->level_one_percentage_premium == 0){
+            $settings->level_one_percentage = 0;
+            $settings->level_one_percentage_standard = 0;
+            $settings->level_one_percentage_premium = 0;
+        }
+        else{
+            $settings->level_one_percentage_premium = $request->level_one_percentage_premium;
+        }
         $settings->level_one_percentage_premium = $request->level_one_percentage_premium;
         $settings->level_two_percentage_premium = $request->level_two_percentage_premium;
-        // $settings->level_three_percentage_premium = $request->level_three_percentage_premium;
+        $settings->level_three_percentage_premium = $request->level_three_percentage_premium;
         $settings->admin_amount_premium = $request->admin_amount_premium;
         $settings->premium_amount = $request->premium_amount;
         $settings->upgrade_wallet_amount_premium = $request->upgrade_wallet_amount_premium;
         $settings->save();
-
         Session::flash('success','Saved!!');
         return redirect()->back();
         
     }
-
     public function saveFacilitation(Request $request){
         if(!$settings = Settings::first()){
             $settings = new Settings;
         }
-
         $settings->facilitation_percentage = $request->facilitation_percentage;
         $settings->save();
-
         Session::flash('success','Saved!!');
         return redirect()->back();
     }
-
     public function CauseDelete($id){
         $cause = Cause::find($id);
         $cause->delete();
@@ -112,7 +93,6 @@ class SettingsController extends Controller
         Session::flash('success','Deleted');
         return redirect()->back();
     }
-
     public function CauseSave(Request $request){
         $cause = new Cause;
         $cause->name = $request->cause;
