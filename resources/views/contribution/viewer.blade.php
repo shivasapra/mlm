d6ba1183a1edd677c2143beb8f9be00afd78c534
shivasapra <ashishsapra7@gmail.com>
2019-08-09 18:41:07 +0530

    Changes

--- resources/views/contribution/viewer.blade.php
+++ resources/views/contribution/viewer.blade.php
@@ -2,33 +2,50 @@
 @extends('layouts.app', ['titlePage' => __('Contribution Viewer')])
 @section('content-body')
     <h1>Contribution Viewer</h1><hr>
-    @if($user->coordinates != null)
-        <div class="tree-view">
-            <ul style="margin-top: 20px;">
-                <li><a href="#" class="addBorderBefore" style="font-weight: normal; color: rgb(66, 139, 202);">{{$user->details->username}}</a>
-                    <ul style="margin-top: 20px;">
-                        @foreach(collect($user->findChildren($user->id)) as $name)
-                            <li style="border-left: 1px solid gray;"><a href="#" style="font-weight: normal; color: rgb(66, 139, 202);"><span class="@if($name != 'N/A') @if(Details::where('username',$name)->first()->invited_by == $user->details->username) text-danger @endif @endif">{{$name}}</span></a>
-                                @if($name != 'N/A')
-                                    <ul style="border-left: 1px solid gray; margin-top: 30px;">
-                                        @foreach(collect($user->findChildren(Details::where('username',$name)->first()->user->id)) as $c)
-                                            <li ><a href="#"><span class="@if($c != 'N/A') @if(Details::where('username',$c)->first()->invited_by == $user->details->username) text-danger @endif @endif">{{$c}}</span></a>
-                                                @if($c != 'N/A')
-                                                    <ul style="border-left: 1px solid gray; margin-top: 30px;">
-                                                        @foreach(collect($user->findChildren(Details::where('username',$c)->first()->user->id)) as $d)
-                                                            <li><a href="#"><span class="@if($d != 'N/A') @if(Details::where('username',$d)->first()->invited_by == $user->details->username) text-danger @endif @endif">{{$d}}</span></a></li>
-                                                        @endforeach
-                                                    </ul>
-                                                @endif
-                                            </li>
-                                        @endforeach
-                                    </ul>
-                                @endif
+
+    <div class="row">
+        <div class="col-md-4">
+            <div class="heading-tag">Basic</div>
+            <div class="border-right">
+                @if($user->coordinates != null)
+                    <div class="tree-view">
+                        <ul style="margin-top: 20px;">
+                            <li><a href="#" class="addBorderBefore" style="font-weight: normal; color: rgb(66, 139, 202);">{{$user->details->username}}</a>
+                                <ul style="margin-top: 20px;">
+                                    @foreach(collect($user->findChildren($user->id)) as $name)
+                                        <li style="border-left: 1px solid gray;"><a href="#" style="font-weight: normal; color: rgb(66, 139, 202);"><span class="@if($name != 'N/A') @if(Details::where('username',$name)->first()->invited_by == $user->details->username) text-danger @endif @endif">{{$name}}</span></a>
+                                            @if($name != 'N/A')
+                                                <ul style="border-left: 1px solid gray; margin-top: 30px;">
+                                                    @foreach(collect($user->findChildren(Details::where('username',$name)->first()->user->id)) as $c)
+                                                        <li ><a href="#"><span class="@if($c != 'N/A') @if(Details::where('username',$c)->first()->invited_by == $user->details->username) text-danger @endif @endif">{{$c}}</span></a>
+                                                            @if($c != 'N/A')
+                                                                <ul style="border-left: 1px solid gray; margin-top: 30px;">
+                                                                    @foreach(collect($user->findChildren(Details::where('username',$c)->first()->user->id)) as $d)
+                                                                        <li><a href="#"><span class="@if($d != 'N/A') @if(Details::where('username',$d)->first()->invited_by == $user->details->username) text-danger @endif @endif">{{$d}}</span></a></li>
+                                                                    @endforeach
+                                                                </ul>
+                                                            @endif
+                                                        </li>
+                                                    @endforeach
+                                                </ul>
+                                            @endif
+                                        </li>
+                                    @endforeach
+                                </ul>
                             </li>
-                        @endforeach
-                    </ul>
-                </li>
-            </ul>
+                        </ul>
+                    </div>
+                @endif
+            </div>
         </div>
-    @endif
+        <div class="col-md-4">
+            <div class="heading-tag standard-gradient">Standard</div>
+            <div class="border-right">
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="heading-tag premium-gradient">Premium</div>
+        </div>
+    </div>
+    
 @endsection
