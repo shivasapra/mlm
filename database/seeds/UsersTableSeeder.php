92e3768e98560ffa03d1f832a3d01a9769be3277
shivasapra <ashishsapra7@gmail.com>
2019-08-13 17:46:02 +0530

    Epins Changes

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,21 +13,21 @@ class UsersTableSeeder extends Seeder
     public function run(Generator $faker)
     {
         $user = new App\User;
-        $user->name = 'Shiva Sapra';
-        $user->email = 'shivasapra24@gmail.com';
+        $user->name = 'Galaxy Crowd';
+        $user->email = 'galaxycrowd@gmail.com';
         $user->admin = 1;
         $user->password = bcrypt('password');
         $user->save();
 
         $detail = new App\Details;
         $detail->user_id = $user->id;
-        $detail->username = 'shivasapra';
-        $detail->full_name = 'Shiva Sapra';
+        $detail->username = 'galaxycrowd';
+        $detail->full_name = 'Galaxy Crowd';
         $detail->country = 'India';
-        $detail->mobile = '9027289261';
+        $detail->mobile = '123456789';
         $detail->invited_by = 'testing';
         $detail->invited_by_email = 'testing@gmail.com';
-        $detail->promotional_url = 'http://galaxycrowd.com/shivasapra';
+        $detail->promotional_url = 'http://galaxycrowd.com/galaxycrowd';
         $detail->security_pin = mt_rand(1000000, 9999999);
         $detail->save();
 
@@ -50,8 +50,8 @@ class UsersTableSeeder extends Seeder
             $detail->full_name = $user->name;
             $detail->country = $faker->country;
             $detail->mobile =  mt_rand(1000000, 9999999);
-            $detail->invited_by = 'shivasapra';
-            $detail->invited_by_email = 'shivasapra24@gmail.com';
+            $detail->invited_by = 'galaxycrowd';
+            $detail->invited_by_email = 'galaxycrowd@gmail.com';
             $detail->promotional_url = 'http://galaxycrowd.com/'.$detail->username;
             $detail->security_pin = mt_rand(1000000, 9999999);
             $detail->save();
