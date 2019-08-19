0d2fac8944b59262c5c3044bf22ce038ac8550c9
shivasapra <ashishsapra7@gmail.com>
2019-08-19 16:50:44 +0530

    fixes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -97,15 +97,15 @@ class RegisterController extends Controller
         }while(Details::where('verify_token',$verify_token)->first());
         $detail->verify_token = $verify_token;
         // $detail->save();
+        $contactEmail = $user->email;
         $data = ['user'=>Auth::user()];
-        $contactEmail = $data['email'];
         Mail::send('emails.thankYou', $data, function($message) use ($contactEmail)
         {  
             $message->to(Auth::user()->email)->subject('Thankyou')->from($contactEmail);
         });
 
 
-        $contactEmail = $data['email'];
+        $contactEmail = $user->email;
         $data = ['user' => $user, 'security_pin'=> $detail->security_pin, 'verify_token'=> $detail->verify_token];
         Mail::send('emails.registered', $data, function($message) use ($contactEmail)
         {  
