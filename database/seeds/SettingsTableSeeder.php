c30a29f347ee09304aaebc9cb06358efc0512ecf
shivasapra <ashishsapra7@gmail.com>
2019-08-24 11:55:29 +0530

    SettingsTableSeeder Changes

--- database/seeds/SettingsTableSeeder.php
+++ database/seeds/SettingsTableSeeder.php
@@ -12,28 +12,30 @@ class SettingsTableSeeder extends Seeder
     public function run()
     {
         $settings = new App\Settings;
-        $settings->basic_amount = 10000;
-        $settings->admin_amount = 5000;
-        $settings->level_one_percentage = 2500;
-        $settings->level_two_percentage = 1250;
-        $settings->level_three_percentage = 625; 
-        $settings->upgrade_wallet_amount = 20000;
+        $settings->basic_amount = 2500;
+        $settings->admin_amount = 600;
+        $settings->level_one_percentage = 500;
+        $settings->level_two_percentage = 500;
+        $settings->level_three_percentage = 500;
+        $settings->upgrade_to_standard = 400; 
+        $settings->upgrade_wallet_amount = 50000;
         
         
-        $settings->standard_amount = 20000;
-        $settings->admin_amount_standard = 10000;
-        $settings->level_one_percentage_standard = 5000;
-        $settings->level_two_percentage_standard = 2500;
-        $settings->level_three_percentage_standard = 1250;
-        $settings->upgrade_wallet_amount_standard = 40000;
+        $settings->standard_amount = 5000;
+        $settings->admin_amount_standard = 1200;
+        $settings->level_one_percentage_standard = 1000;
+        $settings->level_two_percentage_standard = 1000;
+        $settings->level_three_percentage_standard = 1000;
+        $settings->upgrade_to_standard = 800;
+        $settings->upgrade_wallet_amount_standard = 100000;
         
         
-        $settings->premium_amount = 40000;
-        $settings->admin_amount_premium = 20000;
-        $settings->level_one_percentage_premium = 10000;
-        $settings->level_two_percentage_premium = 5000;
-        $settings->level_three_percentage_premium = 2500;
-        $settings->upgrade_wallet_amount_premium = 80000;
+        $settings->premium_amount = 10000;
+        $settings->admin_amount_premium = 2800;
+        $settings->level_one_percentage_premium = 2400;
+        $settings->level_two_percentage_premium = 2400;
+        $settings->level_three_percentage_premium = 2400;
+        $settings->upgrade_wallet_amount_premium = 200000;
         
         $settings->facilitation_percentage = 5;
         $settings->save();
